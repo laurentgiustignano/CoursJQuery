e8894444b90f99531975b1d63004d3263942f27e
Laurent Giustignano <laurent.giustignano@free.fr>
2023-10-02 23:02:10 +0200

    Exemple Onglet - version Vanilla

--- src/app.js
+++ src/app.js
@@ -1,6 +1,18 @@
-$("select").on('change', () => {
-    let str = ""
-    $("select option:selected").each((index, elem) => {
+const tabs = document.querySelectorAll('.tabs a')
+
+for (let i = 0; i < tabs.length; i++) {
+    tabs[i].addEventListener('click', function () {
+
+        const li = this.parentNode
+        const div = this.parentNode.parentNode.parentNode;
+
+        if (this.parentNode.classList.contains('active')) {
+            return false
+        }
+        div.querySelector('.tabs .active').classList.remove('active')
+        li.classList.add('active')
+
+        div.querySelector('.tab-content.active').classList.remove('active')
+        div.querySelector(this.getAttribute('href')).classList.add('active')
     })
-    $(".affichage").text(str)
-})
+}
