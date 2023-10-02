3ed5bdab238558b3081bd27a01106651bbd86be5
Laurent Giustignano <laurent.giustignano@free.fr>
2023-10-02 16:13:56 +0200

    Event hover - version Arrow

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const liens = document.querySelectorAll(".tabs  li")
 
-$(".tabs li").hover(function () {
-    $(this).css({'background-color': '#eee1a0', 'font-weight': 'bolder'})
-}, function () {
-    $(this).css({'background-color': '#FFF', 'font-weight': ''})
+$(".tabs li").hover( (elem) => {
+    $(elem.target).css({'background-color': '#eee1a0', 'font-weight': 'bolder'})
+},  (elem) =>  {
+    $(elem.target).css({'background-color': '#FFF', 'font-weight': ''})
 })
