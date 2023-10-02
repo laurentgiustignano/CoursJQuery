5162f69107e53ef9a22de03377fdba6474dcd64c
Laurent Giustignano <laurent.giustignano@free.fr>
2023-10-02 15:55:38 +0200

    Intro - sans jQuery

---
+++ ReadMe.md
@@ -0,0 +1,2 @@
+## Exemples du cours jQuery
+Les principaux exemples du cours sont repris pour les implémenter directement et obtenir une meilleure visualisation du fonctionnement.
---
+++ src/app.js
@@ -0,0 +1,6 @@
+const liens = document.querySelectorAll(".tabs  li")
+
+for(let indice = 0; indice < liens.length; indice++){
+    const lien = liens[indice]
+    lien.style.color = "#8a1538"
+}
