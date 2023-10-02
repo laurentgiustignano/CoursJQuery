0ef356531be287a1d466a658fc3d3ab72aac72b0
Laurent Giustignano <laurent.giustignano@free.fr>
2023-10-02 16:06:09 +0200

    Intro - avec jQuery

--- src/app.js
+++ src/app.js
@@ -1,6 +1,10 @@
 const liens = document.querySelectorAll(".tabs  li")
 
+/*
 for(let indice = 0; indice < liens.length; indice++){
     const lien = liens[indice]
     lien.style.color = "#8a1538"
 }
+*/
+
+$(".tabs li").css('color',"#00FF00")
